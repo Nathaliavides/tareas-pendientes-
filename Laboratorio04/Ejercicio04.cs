f082a3cb4043990c08d79b0886c5960557cecc2a
Nathaliavides <111389462+Nathaliavides@users.noreply.github.com>
2023-04-27 15:32:33 -0600

    Add files via upload

---
+++ Laboratorio04/Ejercicio04.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio04
+{
+    public class Ejercicio04
+    {
+        public int[] CalcularBilletes(int cantidad, int[] billetes)
+        {
+            int [] result = new int[billetes.Length];
+
+            for (int i = 0; i < billetes.Length; i++) 
+            {
+                result[i] = cantidad / billetes[i];
+                cantidad = cantidad - (result[i] * billetes[i]);
+            }
+            return new int[] {};
+        }
+    }
+}
---
+++ Laboratorio04/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Laboratorio04
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre: ");
+            Console.WriteLine("Carnet: ");
+            Console.WriteLine("Seccion: ");
+        }
+    }
+}
